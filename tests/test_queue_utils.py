b2cb3ddcdae599e34de2114730f4748677af314a
tanner <tanner@nanosurfacebio.com>
2020-07-27 18:26:59 +0000

    Added drain_queue to queue_utils

--- tests/test_queue_utils.py
+++ tests/test_queue_utils.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-import logging
 import multiprocessing
 import queue
 from queue import Queue
 import time
 
 import pytest
-from stdlib_utils import find_log_message_in_queue
+from stdlib_utils import drain_queue
 from stdlib_utils import is_queue_eventually_empty
 from stdlib_utils import is_queue_eventually_not_empty
-from stdlib_utils import LogMessageNotFoundError
-from stdlib_utils import put_log_message_into_queue
-from stdlib_utils import queue_utils
 from stdlib_utils import safe_get
 from stdlib_utils import SimpleMultiprocessingQueue
 
@@ -128,41 +124,13 @@ def test_safe_get__returns_expected_items():
     assert actual_items == expected_items
 
 
-@pytest.mark.parametrize(
-    "expected_item_number,expected_item,test_decsription",
-    [
-        (1, "message_1", "returns first log message"),
-        (2, {"message": 2}, "returns second log message"),
-    ],
-)
-def test_find_log_message_in_queue__returns_expected_item_from_queue_and_leaves_remaining_items(
-    expected_item_number, expected_item, test_decsription, mocker
-):
-    test_items = ["message_1_string", {"message": 2, "other_key": True}, 3]
+def test_drain_queue__returns_list_of_expected_items__and_ignores_None_objects():
+    expected_items = [100, 200, 300]
 
-    spied_not_empty = mocker.spy(queue_utils, "is_queue_eventually_not_empty")
+    q = Queue()
+    for item in expected_items:
+        q.put(None)
+        q.put(item)
 
-    q = multiprocessing.Queue()
-    for item in test_items:
-        put_log_message_into_queue(logging.INFO, item, q, logging.INFO)
-    assert is_queue_eventually_not_empty(q) is True
-
-    actual = find_log_message_in_queue(q, expected_item)
-    if isinstance(expected_item, str):
-        assert expected_item in actual
-    if isinstance(expected_item, dict):
-        assert actual["message"] == expected_item["message"]
-
-    assert spied_not_empty.call_count == expected_item_number
-
-
-def test_find_log_message_in_queue__raises_error_if_message_not_found():
-    q = multiprocessing.Queue()
-    for i in range(3):
-        put_log_message_into_queue(logging.INFO, i, q, logging.INFO)
-
-    test_item = "fake_item"
-    with pytest.raises(
-        LogMessageNotFoundError, match=f"Log Message: '{test_item}' not found in queue"
-    ):
-        find_log_message_in_queue(q, test_item)
+    actual = drain_queue(q)
+    assert actual == expected_items
